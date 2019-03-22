4ffafaba6366c473f42bc3bc8d1114608aafb2b5
ranotosh <ranotosh.p@transneuron.com>
2019-03-22 14:48:56 +0530

    search_locations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule,GoogleMapsAPIWrapper } from '@agm/core';
 
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyA5vTF2UhwGDRm4dbNLC0Dg5tjKCnthMa8'
+    }) ,
+    FormsModule
   ],
-  providers: [],
+  providers: [GoogleMapsAPIWrapper],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
